9ca957aae00954dda48fda55c144ae3d59b0dc2b
codesyariah122 <pujiermanto@gmail.com>
2023-07-08 07:40:14 +0700

    update pdf viewer styling table footer and reload pdf viewer

--- app/views/layout/dashboard/header.php
+++ app/views/layout/dashboard/header.php
@@ -24,10 +24,6 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.5/jspdf.min.js"></script>
 
-    <script src="https://printjs-4de6.kxcdn.com/print.min.js"></script>
-    <link rel="stylesheet" href="https://printjs-4de6.kxcdn.com/print.min.css" />
-
-
     <title><?=$meta['head_title']?></title>
 
     <link rel="shortcut icon" href="<?=$data['favicon']?>">
